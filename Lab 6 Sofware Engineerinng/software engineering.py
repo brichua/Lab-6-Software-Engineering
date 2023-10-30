c611903bb6afdc2c6c3c035536337922e34f13c7
andrewg0825 <148989376+andrewg0825@users.noreply.github.com>
2023-10-30 15:12:45 -0400

    Add files via upload

--- Lab 6 Sofware Engineerinng/software engineering.py
+++ Lab 6 Sofware Engineerinng/software engineering.py
@@ -5,6 +5,16 @@ def encoder(password):
         new_password = new_password + str(int(i) + 3)
     return new_password
 
+#decode password - Andrew Grachoff
+def decoder(encodedPassword):
+    decodedPassword = []
+    for c in encodedPassword:
+        if int(c) > 3:
+            decodedPassword.append(str(int(c) - 3))
+        else:
+            decodedPassword.append(str(int(c) + 7))
+
+    return "".join(decodedPassword)
 
 #main menu display and option action- Brianna Chua
 if __name__ == '__main__':
@@ -23,8 +33,8 @@ if __name__ == '__main__':
             encoded_password = encoder(password)
             print("Your password has been encoded and stored!")
             continue
-        if user_input == 1:
-            #print("The encoded password is "+ encoded_password + " and the original password is " + decoder(encoded_password))
+        if user_input == 2:
+            print("The encoded password is "+ encoded_password + " and the original password is " + decoder(encoded_password))
             continue
-        if user_input == 1:
+        if user_input == 3:
             break
